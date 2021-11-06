f76179e3bcdecd063cc2c87b03fbbf54e29b546b
Yasir Bilal <yasirbilal22@gmail.com>
2021-11-07 00:47:36 +0500

    fixed routing problem

--- src/components/exercise/Addexercise.js
+++ src/components/exercise/Addexercise.js
@@ -24,18 +24,18 @@ function Addexercise({add, cancel}) {
     return (
         <> <div id="wrapper">
         {/* <Navbar/> */}
-            <div class="content-page">
+            <div className="content-page">
      
-            <div class="content">
-                <div class="container-fluid">
+            <div className="content">
+                <div className="container-fluid">
 
-                    <div class="row">
-                        <div class="col-sm-12">
-                            <div class="page-title-box">
-                                <h4 class="page-title">Add New Exercise</h4>
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                                    <li class="breadcrumb-item active">New Exercise</li>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className="page-title-box">
+                                <h4 className="page-title">Add New Exercise</h4>
+                                <ol className="breadcrumb">
+                                    <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                                    <li className="breadcrumb-item active">New Exercise</li>
                                 </ol>
                             </div>
                         </div>
@@ -43,18 +43,18 @@ function Addexercise({add, cancel}) {
                  
 
 
-                    <div class="row justify-content-center">
-                        <div class="col-lg-8">
-                            <div class="card m-b-20">
-                                <div class="card-body">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                            <div className="card m-b-20">
+                                <div className="card-body">
 
-                                    <h4 class="mt-0 header-title">New Exercise</h4>
+                                    <h4 className="mt-0 header-title">New Exercise</h4>
 
                                     <form action="#">
-                                        <div class="form-group row">
-                                            <label class="col-sm-2 col-form-label">Category</label>
-                                            <div class="col-sm-10">
-                                                <select class="form-control" value={activityType}
+                                        <div className="form-group row">
+                                            <label className="col-sm-2 col-form-label">Category</label>
+                                            <div className="col-sm-10">
+                                                <select className="form-control" value={activityType}
                     onChange={e => setActivityType(e.target.value)}>
                                                     <option>Select</option>
                                                     <option>Cycling</option>
@@ -66,18 +66,18 @@ function Addexercise({add, cancel}) {
 
                                         </div>
 
-                                        <div class="form-group row">
-                                            <label for="date-input" class="col-sm-2 col-form-label">Date</label>
-                                            <div class="col-sm-10">
-                                                <input class="form-control" type="date" id="date-input"/>
+                                        <div className="form-group row">
+                                            <label for="date-input" className="col-sm-2 col-form-label">Date</label>
+                                            <div className="col-sm-10">
+                                                <input className="form-control" type="date" id="date-input"/>
                                             </div>
 
                                         </div>
 
-                                        <div class="form-group row">
-                                            <label for="sets" class="col-sm-2 col-form-label">No. of Sets</label>
-                                            <div class="col-sm-10">
-                                                <input class="form-control" type="tel" id="sets" value={sets}
+                                        <div className="form-group row">
+                                            <label for="sets" className="col-sm-2 col-form-label">No. of Sets</label>
+                                            <div className="col-sm-10">
+                                                <input className="form-control" type="tel" id="sets" value={sets}
                     onChange={e => setSets(e.target.value)} 
                                                 
                                                 
@@ -88,23 +88,23 @@ function Addexercise({add, cancel}) {
 
 
 
-                                        {/* <div class="form-group row">
-                                            <label for="time" class="col-sm-2 col-form-label">Starting
+                                        {/* <div className="form-group row">
+                                            <label for="time" className="col-sm-2 col-form-label">Starting
                                                 at</label>
-                                            <div class="col-sm-10">
-                                                <input class="form-control" type="time" value="" id="time"/>
+                                            <div className="col-sm-10">
+                                                <input className="form-control" type="time" value="" id="time"/>
                                             </div>
 
                                         </div> */}
 
 
 
-                                        <div class="form-group row">
-                                            <label for="datetime-local-input" class="col-sm-2 col-form-label">Duration
+                                        <div className="form-group row">
+                                            <label for="datetime-local-input" className="col-sm-2 col-form-label">Duration
                                                 in minutes
                                             </label>
-                                            <div class="col-sm-10">
-                                                <select class="form-control" value={duration}
+                                            <div className="col-sm-10">
+                                                <select className="form-control" value={duration}
                     onChange={e => setDuration(e.target.value)}>
                                                     <option>Select</option>
                                                     <option>5</option>
@@ -120,12 +120,12 @@ function Addexercise({add, cancel}) {
 
 
 
-                                        <div class="form-group row">
+                                        <div className="form-group row">
 
-                                            <label for="datetime-local-input" class="col-sm-2 col-form-label">Reminder
+                                            <label for="datetime-local-input" className="col-sm-2 col-form-label">Reminder
 
                                             </label>
-                                            <div class="col-sm-10">
+                                            <div className="col-sm-10">
                                                 <input type="checkbox" id="switch3" switch="bool" checked 
                                                 value={date}
                                                 onChange={e => setDate(e.target.value)}
@@ -137,14 +137,14 @@ function Addexercise({add, cancel}) {
 
 
 
-                                        <div class="form-group row">
+                                        <div className="form-group row">
 
                                             <label for="datetime-local-input"
-                                                class="col-sm-2 col-form-label">Description
+                                                className="col-sm-2 col-form-label">Description
 
                                             </label>
-                                            <div class="col-sm-10">
-                                                <textarea id="textarea" class="form-control" maxlength="225" rows="3"
+                                            <div className="col-sm-10">
+                                                <textarea id="textarea" className="form-control" maxlength="225" rows="3"
                                                     placeholder="Write any Description here" 
                                                     value={discription}
                                                 onChange={e => setDiscription(e.target.value)}
@@ -153,11 +153,11 @@ function Addexercise({add, cancel}) {
                                             </div>
 
                                         </div>
-                                        <div class="form-group mb-0 row justify-content-center">
+                                        <div className="form-group mb-0 row justify-content-center">
 
                                             <div>
                                                 <button type="submit"
-                                                    class="btn btn-primary waves-effect waves-light mr-1"
+                                                    className="btn btn-primary waves-effect waves-light mr-1"
                                                     disabled={discription === ""} onClick={addItem}
                                                     
                                                     >
@@ -166,7 +166,7 @@ function Addexercise({add, cancel}) {
 
                                                 <Link to="/home">
                                                 <button type="reset" 
-                                                    class="btn btn-secondary waves-effect"
+                                                    className="btn btn-secondary waves-effect"
                                                     onClick={cancel}>
                                                     Cancel
                                                 </button>
